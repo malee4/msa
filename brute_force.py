b884bd12f4df36d4a21547080452cfaffebafb56
Melody Lee <melody.lee357@gmail.com>
2022-06-16 17:56:46 +0000

    testing commits

--- brute_force.py
+++ brute_force.py
@@ -3,3 +3,6 @@ import numpy as np # for pre- and post-processing
 from dimod import BinaryQuadraticModel
 from dwave.system import DWaveSampler, EmbeddingComposite
 
+# inputs: 2D np array with pairwise scores
+
+# objective: select one pairing for each
--- msa.py
+++ msa.py
@@ -5,6 +5,9 @@ from dwave.system import DWaveSampler, EmbeddingComposite
 
 # insert inputs
 strands = [[]] # iterable that stores DNA strands as lists
+# number of columns 
+columns = 20 # currently some arbitrary value
+
 # create pairwise weights
 
 # final weights matrix; strand 1 x original index of base x strand 2 x original index of base [weight]
@@ -32,8 +35,12 @@ bqm = BinaryQuadraticModel('BINARY') # {0, 1}
 
 # incorporate objective function
 ## rewarding term
-for s in range(len(strands)):
-    for i in range(len(strands) - s):
-
+for s_1 in range(len(strands)):
+    for s_2 in range(len(strands) - s_1):
+        for n_1 in range(len(strands[s_1])):
+            for n_2 in range(len(strands[s_2])):
+                for i in range(columns):
+                    
+        
 ## penalty score
 
